b2dee6f84cf59bc9603a4c8a3e0e81de5e78b3ec
RAKHYUN <kyle2598@gmail.com>
2023-11-27 13:57:07 -0600

    Initial Commit

---
+++ PracticeandLearn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeandLearn
+//
+//  Created by Rakhyun Kim on 11/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeandLearn/PracticeandLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeandLearnApp.swift
+//  PracticeandLearn
+//
+//  Created by Rakhyun Kim on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeandLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
